9666e063fb88887048a24c2a69607f879841aa8e
Abbassi Zied <abbassizied@outlook.fr>
2025-07-20 13:39:00 +0100

    first run..

--- discovery-service/src/main/java/io/github/abbassizied/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/io/github/abbassizied/DiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package io.github.abbassizied;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // Enables Eureka Server
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
---
+++ service-b/src/main/java/io/github/abbassizied/web/GlobalPropsController.java
@@ -0,0 +1,28 @@
+package io.github.abbassizied.web;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service-b")
+public class GlobalPropsController {
+	@Value("${global.params.p1}")
+	private String p1;
+
+	@Value("${global.params.p2}")
+	private String p2;
+
+	@GetMapping("/props")
+	public String getProperty() {
+		return "p1=" + p1 + ", p2=" + p2;
+	}
+
+	@GetMapping("/config-maps")
+	public Map<String, String> configTest() {
+		return Map.of("p1", p1, "p2", p2);
+	}
+}
